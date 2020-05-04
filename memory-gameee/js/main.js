2ae3a2736893e6291adea3ce46fb8ecbbf84d005
maliccny <maliccny@gmail.com>
2020-05-03 20:51:42 -0400

    created game logic

--- memory-gameee/js/main.js
+++ memory-gameee/js/main.js
@@ -13,17 +13,37 @@
 // alert('Hello, friends.');
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
+        console.log("You found a match!");
     } else {
-        alert("Sorry, try again.");
+        console.log("Sorry, try again.");
     }
 }
+
+function flipCard(cardId) {
+    // let cardOne = cards[0];
+    // cardsInPlay.push(cardOne);
+    // console.log("User flipped " + cardOne);
+
+    // let cardTwo = cards[2];
+    // cardsInPlay.push(cardTwo);
+    // console.log("User flipped " + cardTwo);
+
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        // if (cardsInPlay[0] === cardsInPlay[1]) {
+        //     alert("You found a match!");
+        // } else {
+        //     alert("Sorry, try again.");
+        // }
+        checkForMatch();
+    }
+}
+
+flipCard(0);
+flipCard(2);
+
+
